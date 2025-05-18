3f8a418bf6507223be887320530085a869198414
driftK1ng <>
2025-05-19 01:10:36 +0300

    Task_1: fix solution and create tests

--- task_1/solution.py
+++ task_1/solution.py
@@ -1,16 +1,39 @@
 import inspect
 
-def before_func(values, input_types):
+def before_func(values: dict, input_types: dict):
+    """Проверяет, соответстуют ли типы параметров функции, типам в аннотации функции
+
+    Args:
+        values (dict): Параметры проверяемой функции
+        input_types (dict): Типы данных в аннотации
+    """
     for key in values.keys():
         if not isinstance(values[key], input_types[key]):
-            raise TypeError(f"Parameter ( {key} ) must be {input_types[key]} not {type(values[key])}")
+            raise TypeError(f"Parameter ( {key} ) " 
+                            f"must be {input_types[key]} not {type(values[key])}")
 
 def after_func(expected_type, result, func_name):
+    """Проверяет возвращаемое значение функции на соответствие, аннотации
+    
+    Args:
+        expected_type (type): Ожидаемый тип данных
+        result (Any): Возвращаемое значение функции
+        func_name (str): Имя функции
+    """
     if not isinstance(result, expected_type):
-        raise TypeError(f"Result of function ( {func_name} ) must be {expected_type} not {type(result)}")
+        raise TypeError(f"Result of function ( {func_name} ) "
+                        f"must be {expected_type} not {type(result)}")
 
 
 def strict(func):
+    """Проверяет, входные и выходные данные на соответствие аннотации
+
+    Args:
+        func (function): Рассматриваемая функция
+
+    Returns:
+        Any: Выходное значение функции
+    """
     sign = inspect.signature(func)
     input_types = func.__annotations__
     def wrapper(*args, **kwargs):
@@ -19,13 +42,4 @@ def strict(func):
         result = func(*args, **kwargs)
         after_func(input_types['return'], result, func.__name__)
         return result
-
     return wrapper
-
-@strict
-def sum_two(a: int, b: int) -> int:
-    return str(a + b)
-
-@strict
-def sum_three(a: int, b: int, c: str) -> int:
-    return a + b
