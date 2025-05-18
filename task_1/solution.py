f671ff0ea7607086d72729ae5e9347f5312de561
driftK1ng <>
2025-05-19 00:28:28 +0300

    Task_1: complete task

--- task_1/solution.py
+++ task_1/solution.py
@@ -1,25 +1,31 @@
 import inspect
 
+def before_func(values, input_types):
+    for key in values.keys():
+        if not isinstance(values[key], input_types[key]):
+            raise TypeError(f"Parameter ( {key} ) must be {input_types[key]} not {type(values[key])}")
+
+def after_func(expected_type, result, func_name):
+    if not isinstance(result, expected_type):
+        raise TypeError(f"Result of function ( {func_name} ) must be {expected_type} not {type(result)}")
+
 
 def strict(func):
-    data = func.__annotations__
-    return_type = data.pop("return")
-
+    sign = inspect.signature(func)
+    input_types = func.__annotations__
     def wrapper(*args, **kwargs):
-        for expected, real in zip(data.values(), args):
-            if (expected != type(real)):
-                raise TypeError
-        value = func(*args)
-        if return_type != type(value):
-            raise TypeError
-        return value
+        values = sign.bind(*args, **kwargs).arguments
+        before_func(values, input_types)
+        result = func(*args, **kwargs)
+        after_func(input_types['return'], result, func.__name__)
+        return result
+
     return wrapper
 
 @strict
 def sum_two(a: int, b: int) -> int:
+    return str(a + b)
+
+@strict
+def sum_three(a: int, b: int, c: str) -> int:
     return a + b
-
-
-print(sum_two(1, 2))  # >>> 3
-#print(sum_two(1, 2.4))  # >>> TypeError
-print(sum_two(1, 4))
