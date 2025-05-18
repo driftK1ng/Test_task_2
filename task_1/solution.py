b451251f3f8e2d2e8ec01c4ed60103853841841a
driftK1ng <>
2025-05-18 21:24:42 +0300

    Task_1: added strict check for args

---
+++ task_1/solution.py
@@ -0,0 +1,25 @@
+import inspect
+
+
+def strict(func):
+    data = func.__annotations__
+    return_type = data.pop("return")
+
+    def wrapper(*args, **kwargs):
+        for expected, real in zip(data.values(), args):
+            if (expected != type(real)):
+                raise TypeError
+        value = func(*args)
+        if type(value) != return_type:
+            raise TypeError
+        return value
+    return wrapper
+
+@strict
+def sum_two(a: int, b: int) -> int:
+    return a + b
+
+
+print(sum_two(1, 2))  # >>> 3
+#print(sum_two(1, 2.4))  # >>> TypeError
+print(sum_two(1, 4))
